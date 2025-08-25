ef3d606af91cfe72848fcf79f8be1db62594e3d1
DanielShopee <daniel.sbatista@shopee.com>
2025-08-25 12:12:24 -0300

    feat: The new model was added to project.

--- src/fast_zero/app.py
+++ src/fast_zero/app.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from src.fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_app():
-    return {'message': 'Hello World!'}
+    return Message(message="Hello Worlds!")
